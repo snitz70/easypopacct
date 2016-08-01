dfd5c608786410260bb8238cb734ed153c1ed2cf
Brian Snyder <bsnyder@technology4retailers.com>
2016-08-01 12:27:26 -0500

    initial commit

--- easypopacct/Program.cs
+++ easypopacct/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace easypopacct
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            int acct = Convert.ToInt16(args[0]);
+            acct -= 2000;
+            string acct_text = Convert.ToString(acct);
+            Clipboard.SetText(acct_text);
+            
         }
     }
 }
